19f3f9e2fd0408966ac0d89c037926bde611154c
MinGyu, Ju <knighp@gmail.com>
2021-05-26 13:29:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love you Pink A!');
+    res.send('I love you IU!');
 });
 
 app.get('/start', function(req, res){
